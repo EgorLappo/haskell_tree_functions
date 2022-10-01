a719569a37b7fcdecf313f7cbf9f3e5748341815
Egor Alimpiev <alimpiev@stanford.edu>
2022-10-01 22:19:47 +0200

    prreparing to rewrite the graph part with arrays

--- src/ACLattices.hs
+++ src/ACLattices.hs
@@ -1,20 +1,32 @@
-module ACLattices where
+{-# LANGUAGE TupleSections #-}
+
+module ACLAttices ( Vertex
+                  , AC
+                  , EdgeList
+                  , AdjList
+                  , getEdgeList
+                  , getAdjacencyList
+                  )  where
 
 import Control.Lens
 import qualified Data.HashMap.Strict as M
-import Data.HashMap.Strict (HashMap, (!))
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as H
 
 import Tree
 import AncestralConfigurations ( matchingRootConfigurations, showAC )
 import Data.Maybe (fromJust)
-import Data.List (nub, sortOn)
+import Data.List (nub, sort, sortOn)
+import qualified Data.Array as A
+import Data.Array (Array, (!))
 
-type AC = [String]
+type AC = [String] 
 data Vertex = V
     { getAC :: AC
     , getLabel :: Int } deriving (Eq, Show)
 type EdgeList = [(Vertex, Vertex)]
 type AdjList = [(Vertex, [Vertex])]
+type Path = [Vertex]
 
 -- I wrote these functions at 2 am so they are insane one-liners. here is how it works
 -- 1 - select non-leaf lineage from an AC. they can be "split" into two of their children nodes to get one of the new ACs covered by the current one
@@ -24,13 +36,31 @@ getPrecedingACs t ac = map (\x -> replaceElementAndFlatten ac x (fromJust $ chil
   filter (not . isLeafLabel t) ac
 
 getEdgeList :: Tree String -> EdgeList
-getEdgeList t = map (\(x, y) -> (V x (labels ! x) , V y (labels ! y))) $
+getEdgeList t = map (\(x, y) -> (V x (labels H.! x) , V y (labels H.! y))) $
                      concatMap (\ac -> map (,ac) $ getPrecedingACs t ac) aclist
-    where aclist = matchingRootConfigurations t
+          -- we sort the lineages in the ACs to ensure consistent representatiion
+    where aclist = map sort $ matchingRootConfigurations t 
           labels = M.fromList $ zip aclist [1..]
  
+getAdjacencyList :: Tree String -> AdjList
+getAdjacencyList = toAdjacencyList . getEdgeList
 
-getAdjacencyList :: EdgeList -> AdjList
-getAdjacencyList es = map (\v -> (v, [y | (x,y) <- es, x == v])) $
+toAdjacencyList :: EdgeList -> AdjList
+toAdjacencyList es = map (\v -> (v, [y | (x,y) <- es, x == v])) $
     sortOn getLabel $ nub $ uncurry (++) $ unzip es
+
+toEdgeList :: AdjList -> EdgeList
+toEdgeList = concatMap (\(v, vs) -> map (v,) vs)
+
+cartProd :: EdgeList -> EdgeList -> EdgeList 
+cartProd xs ys = [mkEdge e e' | e <- xs, e' <- ys]
+  where mkEdge (a, b) (a', b') = (mkNode a a', mkNode b b')
+        mkNode a b = V (mkAC a b) (mkLabel a b)
+        mkAC (V l _) (V l' _) = sort (l ++ l')
+        mkLabel (V _ i) (V _ j) = i*n + j
+        n = length $ vertices xs
+
+vertices :: EdgeList -> [Vertex]
+vertices = nub . concat . unzip 
+
 
